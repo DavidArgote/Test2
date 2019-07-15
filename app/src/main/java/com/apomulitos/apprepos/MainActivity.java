1c117a06ccbfb81aae7097a509a3073a8bf6adec
ZULEMA <zleon20@gmail.com>
2019-07-15 17:50:34 -0500

    creaación de layout con botones, creación de metodo referenciar ......."

--- app/src/main/java/com/apomulitos/apprepos/MainActivity.java
+++ app/src/main/java/com/apomulitos/apprepos/MainActivity.java
@@ -1,13 +1,39 @@
 package com.apomulitos.apprepos;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnLuis;
+    private Button btnDaniel;
+    private Button btnCris;
+    private Button btnDavid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        referenciar();
+
+
+    }
+
+    private void referenciar() {
+
+        btnLuis = findViewById(R.id.btnLuis);
+        btnDaniel = findViewById(R.id.btnDaniel);
+        btnCris = findViewById(R.id.btnCristhian);
+        btnDavid = findViewById(R.id.btnDavid);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        
     }
 }
