582a55f90242aa5dcb33468763cb85922abc6f68
CRISTHIAN <criacamu@gmail.com>
2019-07-19 16:46:39 -0500

    Se termina la aplicacion peso ideal by canarindustries@

--- app/src/main/java/com/apomulitos/apprepos/pesoIdealCristhian.java
+++ app/src/main/java/com/apomulitos/apprepos/pesoIdealCristhian.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 
 public class pesoIdealCristhian extends AppCompatActivity implements View.OnClickListener {
 
-    EditText altura, peso;
+    EditText altura, peso, edad;
     TextView resMci, resCons,resPI;
     Button btnCalcular;
 
@@ -23,16 +23,15 @@ public class pesoIdealCristhian extends AppCompatActivity implements View.OnClic
 
 
 
-
+        //Referencias
         altura = findViewById(R.id.etAlturaCristhian);
-
         peso = findViewById(R.id.etPesoCristhian);
-
-
+        edad = findViewById(R.id.etEdadCristhian);
 
         resCons = findViewById(R.id.tvResultadoConsCristhian);
         resMci = findViewById(R.id.tvResultadoMciCristhian);
         resPI = findViewById(R.id.tvResultadoPICristhian);
+
         btnCalcular = findViewById(R.id.btnCalcularCristhian);
         btnCalcular.setOnClickListener(this);
 
@@ -42,38 +41,44 @@ public class pesoIdealCristhian extends AppCompatActivity implements View.OnClic
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.btnCalcularCristhian:
+        String textoEdad = edad.getText().toString();
+        double valorEdad = Double.parseDouble(textoEdad);
+        if (valorEdad >= 20 && valorEdad <=65) {
+            switch (view.getId()) {
+                case R.id.btnCalcularCristhian:
 
-                String textoA = altura.getText().toString();
-                double valorA = Double.parseDouble(textoA);
-                double valorAMci = valorA/100;
+                    String textoA = altura.getText().toString();
+                    double valorA = Double.parseDouble(textoA);
+                    double valorAenMetros = valorA / 100;
 
-                String textoP = altura.getText().toString();
-                double valorP = Double.parseDouble(textoP);
+                    String textoP = peso.getText().toString();
+                    double valorP = Double.parseDouble(textoP);
 
-                double resultadoMci = (valorP/Math.pow(valorAMci,2));
+                    double resultadoMci = (valorP / (valorAenMetros*valorAenMetros));
 
-                if (resultadoMci<18.5){
-                    resCons.setText("Usted tiene un peso demasiado bajo");
-                }else if (resultadoMci >=25){
-                    resCons.setText("Usted tiene SOBREPESO");
-                }else if (resultadoMci > 30){
-                    resCons.setText("Usted tiene OBESIDAD");
-                }
+                    if (resultadoMci < 18.5) {
+                        resCons.setText("Usted tiene un peso Demasiado bajo");
+                    }else if (resultadoMci >= 18.5 && resultadoMci<=24.9 ){
+                        resCons.setText("Usted tiene un peso Normal");
+                    }else if (resultadoMci >= 25 && resultadoMci <=30) {
+                        resCons.setText("Usted tiene SOBREPESO");
+                    } else if (resultadoMci > 30) {
+                        resCons.setText("Usted tiene OBESIDAD");
+                    }
 
-                resMci.setText("Su indice de masa corporal es: "+resultadoMci);
-                double valorPI = (valorA-100);
-                resPI.setText("Su peso ideal debe de ser de: "+valorPI);
+                    resMci.setText("Su indice de masa corporal es: " + resultadoMci);
+                    double valorPI = (valorA - 100);
+                    resPI.setText("Su peso ideal debe de ser de: " + valorPI);
 
-                break;
+                    break;
 
-            default:
-                Toast.makeText(this, "Opcion no valida", Toast.LENGTH_SHORT).show();
+                default:
+                    Toast.makeText(this, "Opcion no valida", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(this, "Su edad no esta en el rango de 20 a 65 para el calculo", Toast.LENGTH_SHORT).show();
         }
-
 
     }
 
 }
-
